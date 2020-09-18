b83ad2533a5e19d12e36138376f73534bd574829
fenguoerbian <413557584@qq.com>
2020-09-18 16:00:54 +0800

    Adjust the documentation about this package.

---
+++ R/RSAVS.r
@@ -0,0 +1,12 @@
+#' RSAVS: A package for robust subgroup analysis and variable selection.
+#' 
+#' The RSAVS can do subgroup analysis and variable selection simultaneously and 
+#' it supports multiple choices of loss and penalty functions.
+#' It implements the computation in a parallel manner.
+#' 
+#' @section RSAVS functions:
+#' RSAVS_LargeN
+#' 
+#' @docType  package
+#' @name RSAVS
+NULL
