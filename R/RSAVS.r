9fbfbf7e0c24b3a724fee082cf6148365d8da313
fenguoerbian <413557584@qq.com>
2021-03-15 16:47:39 +0800

    Update RSAVS.r.

--- R/RSAVS.r
+++ R/RSAVS.r
@@ -5,7 +5,19 @@
 #' It implements the computation in a parallel manner.
 #' 
 #' @section RSAVS functions:
+#' RSAVS_Compute_BIC
+#' RSAVS_Determine_Mu
+#' RSAVS_Further_Improve
+#' RSAVS_Generate_D_Matrix
+#' loss_function: RSAVS_Huber, RSAVS_L1, RSAVS_L2
 #' RSAVS_LargeN
+#' RSAVS_Mu_to_Mat
+#' RSAVS_RI
+#' RSAVS_Summary_Iteration
+#' RSAVS_S_to_Groups
+#' 
+#' @section RSAVS datesets:
+#' Student performance dataset: full_df, mat_df, por_df
 #' 
 #' @docType  package
 #' @name RSAVS
